5ca2a672cab7c7aecdb0623ee0b0519824ebd344
vagrant <vagrant@vagrant.vm>
2017-07-09 01:53:51 +0000

    switching tracks to scraping

--- twitter_search.py
+++ twitter_search.py
@@ -7,10 +7,11 @@ access_token = os.environ['TWITTER_ACCESS_TOKEN_KEY']
 access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
 # authentication block - not final location!
-authorization = tweepy.OAuthHandler(consumer_token, consumer_secret)
-authorization.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 words = ["women", "scholarship"]
-results = tweepy.api.search("scholarship")
+results = api.search("scholarship")
 print(results)
 # api.search(q="scholarship", lang ='en', since_id=1, show_user=True)
